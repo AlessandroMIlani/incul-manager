d5459747c433aabcb1f8cf58b6d88850b20a0a12
AlessandroMilani <alessandromilani2000@gmail.com>
2024-12-10 15:34:19 +0100

    refactor config.sh to restore password variable and remove unused source archive definitions

--- etc/inculs-manager/config.sh
+++ etc/inculs-manager/config.sh
@@ -3,16 +3,3 @@
 username="incul"
 
 password="incul"
-
-# source archive mirror for the containers
-
-new_sources="
-deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
-deb-src http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
-
-deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
-deb-src http://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
-
-deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
-deb-src http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
-"
