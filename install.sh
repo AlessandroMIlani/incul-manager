49d6fdc95d06967b885363c0e419ec99b6965363
AlessandroMilani <alessandromilani2000@gmail.com>
2024-12-10 15:36:45 +0100

    refactor install script to improve package checks

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Function to check if a package is installed
-package_installed() {
-	dpkg -l "$1" &>/dev/null
-}
-
 need_reboot=false
 # Check if the system is Debian 12 with XFCE
 if ! grep -q "Debian GNU/Linux 12" /etc/os-release; then
@@ -17,11 +12,16 @@ if ! echo "$XDG_CURRENT_DESKTOP" | grep -qE "XFCE|KDE|GNOME"; then
     exit 1
 fi
 
+echo "cloning the repo"
+
 git clone -b personal-main https://github.com/AlessandroMIlani/incul-manager.git 
 
 
-if ! package_installed incus; then
+if ! which incus &>/dev/null; then
+    echo "incus not found, installing"
     # Step 1: Setup backports
+
+
     echo -e "\n\e[1;33mSet up backports...\e[0m"
     echo "deb http://deb.debian.org/debian bookworm-backports main" | sudo tee /etc/apt/sources.list.d/bookworm-backports.list >/dev/null
     sudo apt update
@@ -35,6 +35,7 @@ fi
 
 sudo apt install python3-netifaces xpra python3-cups python3-opencv python3-gi-cairo xfce4-notifyd
 
+echo "creating the deb package"
 dpkg --build incul-manager
 sudo dpkg -i incul-manager.deb
 
@@ -45,16 +46,16 @@ CURRENT_USER=$(whoami)
 sudo adduser "$CURRENT_USER" incus-admin
 
 # Step 3: setup menu
-mkdir -p /home/$USER/.local/share/applications
-mkdir -p /home/$USER/.local/share/desktop-directories
-mkdir -p /home/$USER/.config/menus
-mkdir -p /home/$USER/.config/incul-manager
+mkdir -p /home/"$USER"/.local/share/applications
+mkdir -p /home/"$USER"/.local/share/desktop-directories
+mkdir -p /home/"$USER"/.config/menus
+mkdir -p /home/"$USER"/.config/incul-manager
 
 ssh-keygen -t rsa -b 4096 -C "$CURRENT_USER@incus-containers" -f "/home/$USER/.ssh/incul-id_rsa" -N ""
-echo "ssh-add /home/$USER/.ssh/incul-id_rsa > /dev/null 2>&1" >> /home/$USER/.bashrc 
+echo "ssh-add /home/$USER/.ssh/incul-id_rsa > /dev/null 2>&1" >> /home/"$USER"/.bashrc 
 
-sudo cp -r /etc/inculs-manager/launcher-config/desktop-directories /home/$USER/.local/share/
-sudo cp -a /etc/inculs-manager/launcher-config/. /home/$USER/.config/menus/
+sudo cp -r /etc/inculs-manager/launcher-config/desktop-directories /home/"$USER"/.local/share/
+sudo cp -a /etc/inculs-manager/launcher-config/. /home/"$USER"/.config/menus/
 
 if $need_reboot; then
     echo -e "\n\e[1;33mIncul-manager dependence needs restart for work correctly. Press any key to confirm the reboot...\e[0m"
